b916cdeae5ac3e2898c06b6ffcfb7b9e3227943f
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-23 23:07:03 +0530

    Added Valid First Name Program

---
+++ RegexProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RegexProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Regular Expression Program");
+            //UC_1
+            ValidFirstNameUC_1 validFirstName = new ValidFirstNameUC_1();
+            Console.WriteLine("please enter the name");
+
+            string validfirstname = Console.ReadLine();
+
+            bool value = validFirstName.validateValidfirstName(validfirstname);
+            if (value)
+            {
+                Console.WriteLine("Name is matched");
+            }
+            else
+            {
+                Console.WriteLine("Name is not matched");
+            }
+        }
+    }
+}
