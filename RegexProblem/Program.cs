68bd130c30dd3bbe983a91792fd1c87577f93b01
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-23 23:14:24 +0530

    Added Valid Last Name Program

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -7,11 +7,27 @@
             Console.WriteLine("Displaying Regular Expression Program");
             //UC_1
             ValidFirstNameUC_1 validFirstName = new ValidFirstNameUC_1();
-            Console.WriteLine("please enter the name");
+            Console.WriteLine("please Enter The First Name");
 
             string validfirstname = Console.ReadLine();
 
             bool value = validFirstName.validateValidfirstName(validfirstname);
+            if (value)
+            {
+                Console.WriteLine("Name is matched");
+            }
+            else
+            {
+                Console.WriteLine("Name is not matched");
+            }
+            //UC_2
+
+            ValidLastNameUC_2 validLastName2 = new ValidLastNameUC_2();
+            Console.WriteLine("Please Enter The Last Name");
+
+            string validlastname = Console.ReadLine();
+
+            bool value1 = validLastName2.validateValidLastName(validlastname);
             if (value)
             {
                 Console.WriteLine("Name is matched");
